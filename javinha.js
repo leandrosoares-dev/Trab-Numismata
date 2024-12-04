438c9b560aedb4526f1d6572b19d15b1e591113b
Leandro Soares <antcesarsoares35@gmail.com>
2024-12-04 17:56:04 -0300

    Add files via upload

---
+++ javinha.js
@@ -0,0 +1,6 @@
+function enviar(){
+    var nome = document.getElementById("NomeCompleto").value;
+    var email = document.getElementById("E-mail").value;
+
+    alert(nome + " você recebeu uma mensagem em " + email);
+}
